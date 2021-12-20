8777f3412b58fb94fabae9b5b392dbcb8e620038
carlos giner <carlosginer90@gmail.com>
2021-12-20 10:32:24 +0100

    add allow CORS lines

--- app.js
+++ app.js
@@ -33,7 +33,14 @@ app.use(index);
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://meteologica-app-server.herokuapp.com/"); 
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, Access-Control-Allow-Credentials');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 // if (process.env.NODE_ENV === "production"){
 //     app.use(express.static(path.join(__dirname, 'client/build')));
