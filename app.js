23d93807b120b36b7e0a965a0884e40b9d34e28c
carlos giner <carlosginer90@gmail.com>
2021-12-27 16:52:29 +0100

    corrected the reset issue, add comments

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ const YAML = require('js-yaml');
 const path =require('path');
 const cors = require('cors');
 
-
+//configuracion para permiter CORS en el browser
 app.use(cors({
     origin: '*',
     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }));  
 
-app.options('https://meteologica-app-server.herokuapp.com/',(req, res, next) => {
+app.options('https://meteologica-app-server.herokuapp.com',(req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', '*');
     res.header('Access-Control-Allow-Methods', '*');
@@ -26,39 +26,53 @@ app.options('https://meteologica-app-server.herokuapp.com/',(req, res, next) => 
 });
 
 app.use(index);
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended:false}));
+// app.use(bodyParser.json());
 
+//Aqui la data es pasada a JSON para poder trabajar con ella. 
 const rawData = fs.readFileSync('data.yml');
 const data = YAML.load(rawData);
 
-const updatedTemperatures = []
+//se definen los array que serán pasados a traves del socket. Nota: Solo hace falta actualizar temperaturas, pues el rango del tiempo
+//se esta tomando de ellas, pues son identicas. Para rangos de tiempos distintos habria que resetear el array de potencias tambien. 
+let updatedTemperatures = []
 const updatedPowers = [];
-const valuesCount = data.temperature.values.length
+const valuesCount = data.temperature.values.length-1;
+let count=0;
 
-const sendDelayData = async (socket) =>{
-    for (let i = 0; i < valuesCount; i++) {
-        updatedTemperatures.push(data.temperature.values[i]);
-        updatedPowers.push(data.power.values[i]);
-        await new Promise(done => setTimeout(() => done(), 5000))
-        console.log(updatedTemperatures[updatedTemperatures.length-1]);
-        console.log(updatedPowers[updatedPowers.length-1]);
-        socket.emit("FromAPI",{updatedTemperatures,updatedPowers,usersCount});
-        if (i===valuesCount-1){
-            i = 0;
-        }   
-    }
+//cada vez que es invocado, se agrega un objeto con los datos al array especificado. 
+const getData = () =>{
+    updatedTemperatures.push(data.temperature.values[count]);
+    updatedPowers.push(data.power.values[count]);
 }
+
+//Según lo solicitado se actualizan los datos cada 5 segundos. Count sirve para llevar cuenta y que al llegar al final 
+//de los datos (de prueba) proveidos, vuelva a comenzar de 0.  
+setInterval(() => {
+    count=count+1;
+    if (count === valuesCount){
+        count = 0;
+        //!! es sumamente importante mutar el array vaciándolo, pues de no hacerlo, continuaría llenandose el array (push) y enviandose al front,
+        //rompiendo el efecto deseado de continuidad en la grafica cuando se terminan los datos. 
+        updatedTemperatures=[]
+    }
+    getData()
+    io.emit("sendingData",{updatedTemperatures,updatedPowers}); 
+}, 5000);
+
 
 io.on("connection", (socket) => {
 
-    const usersCount = io.sockets.sockets.length;
-    sendDelayData(socket)   
-
+    //esto solo para visualizar en consola cuantos cientes hay conectados y desconectados. Nota: para verlo en el browser hace falta 
+    //una librería adicional no instalada aún. 
+    const usersCount = Object.keys(io.engine.clients).length
+    console.log(`${usersCount} users connected`)
+    
     socket.on("disconnect", () => {
         console.log("Client disconnected");
     });
 });
+
 
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
