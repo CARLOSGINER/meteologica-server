20a5044038cdbf5f0ac97eeac0e061ec82aeccdd
carlos giner <carlosginer90@gmail.com>
2021-12-28 10:22:29 +0100

    updated

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 4001;
 const socketIo = require("socket.io");
 const io = socketIo(server,{
     cors: {
-      origin: "https://61cabe1feb07cbe6070961cb--meteologica-app-front.netlify.app",
+      origin: "https://61cad72bbd57230dd52dae3f--meteologica-app-front.netlify.app",
       methods: ["GET", "POST"],
     },
   });
